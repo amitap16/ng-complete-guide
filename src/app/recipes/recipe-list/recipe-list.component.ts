5d3f94e7e950657b16e2bcb1629ba14fc898b5f7
Amita Patel <amitap16@live.com>
2021-04-20 07:26:07 -0400

    Added Recipe Model and WIP for using Recipe model in Recipe-List component.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Daal Dhokli'
+      , 'Gujarati Sunday Special Dish, yummy!! Dal Dhokli is a spicy, sweet, one-pot meal, prepared by simmering whole wheat flour bits in a lentil-based motley perked with spices. It is a popular dish in Gujarat and Rajasthan and is prepared mostly for lunch.'
+      , 'https://ministryofcurry.com/wp-content/uploads/2017/06/IMG_3071-scaled.jpg')
+  ];
 
   constructor() { }
 
