6abc30c6fb6a8e6b7b3903df7a207688c1abd21a
Amita Patel <amitap16@live.com>
2021-06-15 23:07:02 -0400

    Completed: Lession # 117 - Managing Recipe in Recipe Service. Lession # 118. Using a Service for Cross-Component Communication.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../shared/models/recipe.model';
+import { RecipeService } from '../shared/services/recipe.service';
 
 @Component({
   selector: 'app-recipes',
@@ -9,8 +10,13 @@ import { Recipe } from '../shared/models/recipe.model';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService : RecipeService) { }
 
   ngOnInit(): void {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => {
+        this.selectedRecipe = recipe;
+      }
+    );
   }
 }
